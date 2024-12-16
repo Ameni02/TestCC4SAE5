aadd58b3d98ff49d0655e707ea63c25310a5191c
Ameni02 <ameni.zoubeir@esprit.tn>
2024-12-16 11:21:44 +0100

    questions 1&2&3&4&5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './Components/products/add-product/add-product.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/add', component: AddProductComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { AddProductComponent } from './Components/products/add-product/add-product.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductsComponent,
+    AddProductComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
